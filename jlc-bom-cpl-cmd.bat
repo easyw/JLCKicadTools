babd935d8c57ba5ec7b89651e67fd0addd46ff60
easyw <easyw@katamail.com>
2021-03-04 14:58:09 +0100

    edding batch to launch the tool

--- jlc-bom-cpl-cmd.bat
+++ jlc-bom-cpl-cmd.bat
@@ -1,3 +1,9 @@
-C:\Python3\python C:\Python3\Lib\site-packages\jlc_kicad_tools  C:\Temp\myprj
+echo OFF
+
+SET PRJ=C:\Temp\myprj
+cd %PRJ%
+echo working on %PRJ% project
+
+C:\Python3\python C:\Python3\Lib\site-packages\jlc_kicad_tools %PRJ%
 
 pause
